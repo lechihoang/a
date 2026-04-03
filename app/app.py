cd09a5764e0f571a7173e30f00598b7e1fe8963c
lechihoang <lechihoang73@gmail.com>
2026-04-03 14:08:23 +0700

    test ci/cd

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ load_dotenv()
 # ── Page Config ───────────────────────────────────────────────────────────────
 
 st.set_page_config(
-    page_title="UK Property Analytics",
+    page_title="UK Property Analytics(Ver 1)",
     page_icon="🏠",
     layout="wide",
     initial_sidebar_state="expanded",
